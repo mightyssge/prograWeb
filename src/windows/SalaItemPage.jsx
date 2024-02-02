f679b511aedc6e4d9b919282a41b717a99e2fc89
CesarAndres04 <114117257+CesarAndres04@users.noreply.github.com>
2024-02-02 10:42:55 -0500

    nada

--- src/windows/SalaItemPage.jsx
+++ src/windows/SalaItemPage.jsx
@@ -1,9 +1,23 @@
 import Header from '../components/Cabecera.jsx';
 import { Container,Stack } from "@mui/material";
 import ContentSalaItem from '../components/ContentSalaItem.jsx'
-
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SalaItemPage() {
+
+  const navigate = useNavigate();
+
+  // Verifica si el usuario está autenticado al cargar la página de películas
+  useEffect(() => {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+
+    if (!isLoggedIn) {
+      // Si no está autenticado, redirige al login
+      navigate('/login');
+    }
+  }, [navigate]);
+
     return (
 
       //esto es el contenido de los menus
