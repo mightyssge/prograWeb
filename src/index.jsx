6aa7a2b32ca1acb6b6b9f29d21054e6cbf60e4a6
mightyssge <118016003+mightyssge@users.noreply.github.com>
2024-02-18 02:42:17 -0500

    CAMBIO

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD:src/index.jsx
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js'
+=======
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.jsx'
+>>>>>>> parent of 4f1179e (a):src/index.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
